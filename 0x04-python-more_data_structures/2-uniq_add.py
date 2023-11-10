a0b1b8b080e5ad9f584e5075429b7722bcd833cd
ammar1282009 <ammar1282009@outlook.com>
2023-11-10 05:19:29 -0800

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    # Create a new list to store the modified elements
     new_list = []
-
-    # Iterate over each element in the original list
     for item in my_list:
-        # Replace the element if it matches the 'search' value
         if item == search:
             new_list.append(replace)
         else:
